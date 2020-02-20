c6c7d94b4f48cb375276843cc3daf1e0bd72a2b0
mhrrmk <mkocadere@gmail.com>
2020-02-20 23:05:59 +0300

    <fix Container depreciated. improve Header html. improve Header Title css

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
 import React from "react"
 
-export function Header(props){
+export function Header(props) {
     return (
-        <div className="header">header</div>
+        <div className="header">
+            <div className="content header-content">
+                <div className="DEMO-Streaming">DEMO Streaming</div>
+                <div className="header-buttons">
+                    <a className="login">login</a>
+                    <a className="trial">
+                        <div className="trial-text">Start Your Free Trial</div>
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
--- components/Template.js
+++ components/Template.js
@@ -1,9 +1,13 @@
 import React from "react"
+import Head from 'next/head';
 import {Header, Footer} from "."
 
 export function Template({children}){
     return (
         <div className="container">
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"/>
+            </Head>
             <Header/>
             {children}
             <Footer/>
