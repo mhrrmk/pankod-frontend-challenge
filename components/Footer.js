ecb72d09d98cec83c6c24f1f1b1a47c6d33039f4
mhrrmk <mkocadere@gmail.com>
2020-02-20 23:54:28 +0300

    improve footer html structure

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,38 @@
 import React from "react"
 
-export function Footer(props){
+export function Footer(props) {
     return (
-        <div className="footer">footer</div>
+        <div className="footer">
+            <div className="content footer-content">
+                <div className="footer-text-content">
+                    <div className="footer-links">
+                        <a className="footer-link-item">Home</a>
+                        <span className="footer-link-item">|</span>
+                        <a className="footer-link-item">Terms and Conditions</a>
+                        <span className="footer-link-item">|</span>
+                        <a className="footer-link-item">Privacy Policy</a>
+                        <span className="footer-link-item">|</span>
+                        <a className="footer-link-item">Collection Statement</a>
+                        <span className="footer-link-item">|</span>
+                        <a className="footer-link-item">Help</a>
+                        <span className="footer-link-item">|</span>
+                        <a className="footer-link-item">Manage Account</a>
+                    </div>
+                    <div className="copyright">Copyright © 2016 DEMO Streaming. All Rights Reserved.</div>
+                </div>
+                <div className="footer-buttons">
+                    <div className="social">
+                        <img className="footer-social-button-item" src="/assets/social/facebook-white.svg" />
+                        <img className="footer-social-button-item" src="/assets/social/instagram-white.svg" />
+                        <img className="footer-social-button-item" src="/assets/social/twitter-white.svg" />
+                    </div>
+                    <div className="store">
+                        <img className="footer-store-button-item" src="/assets/store/app-store.svg" />
+                        <img className="footer-store-button-item" src="/assets/store/play-store.svg" />
+                        <img className="footer-store-button-item" src="/assets/store/windows-store.svg" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
