2ed48a4c33f7c708650c54536446296823c19cdc
mhrrmk <mkocadere@gmail.com>
2020-02-22 18:47:35 +0300

    style Footer using Styled Components

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,41 +1,43 @@
 import React from "react"
 import {
     FooterWrapper,
-    FooterContent
+    FooterContent,
+    FooterTextContent,
+    FooterLinks,
+    Copyright,
+    FooterButtons,
+    Social,
+    Store
 } from "./Styled"
+import {SocialButton, StoreButton, FooterLink} from ".."
+import {copyright, footerLinks} from "../../Constants"
 
 export function Footer(props) {
     return (
         <FooterWrapper>
             <FooterContent>
-                <div className="footer-text-content">
-                    <div className="footer-links">
-                        <a className="footer-link-item">Home</a>
-                        <span className="footer-link-item">|</span>
-                        <a className="footer-link-item">Terms and Conditions</a>
-                        <span className="footer-link-item">|</span>
-                        <a className="footer-link-item">Privacy Policy</a>
-                        <span className="footer-link-item">|</span>
-                        <a className="footer-link-item">Collection Statement</a>
-                        <span className="footer-link-item">|</span>
-                        <a className="footer-link-item">Help</a>
-                        <span className="footer-link-item">|</span>
-                        <a className="footer-link-item">Manage Account</a>
-                    </div>
-                    <div className="copyright">Copyright © 2016 DEMO Streaming. All Rights Reserved.</div>
-                </div>
-                <div className="footer-buttons">
-                    <div className="social">
-                        <img className="footer-social-button-item" src="/assets/social/facebook-white.svg" />
-                        <img className="footer-social-button-item" src="/assets/social/instagram-white.svg" />
-                        <img className="footer-social-button-item" src="/assets/social/twitter-white.svg" />
-                    </div>
-                    <div className="store">
-                        <img className="footer-store-button-item" src="/assets/store/app-store.svg" />
-                        <img className="footer-store-button-item" src="/assets/store/play-store.svg" />
-                        <img className="footer-store-button-item" src="/assets/store/windows-store.svg" />
-                    </div>
-                </div>
+                <FooterTextContent>
+                    <FooterLinks>
+                        {footerLinks.map((item, i, array) => (
+                            i !== array.length - 1 ?
+                            <FooterLink key={i} title={item.title} link={item.link}/> :
+                            <FooterLink key={i} title={item.title} link={item.link} last={true}/>
+                        ))}
+                    </FooterLinks>
+                    <Copyright>{copyright}</Copyright>
+                </FooterTextContent>
+                <FooterButtons>
+                    <Social>
+                        <SocialButton src="/assets/social/facebook-white.svg" />
+                        <SocialButton src="/assets/social/instagram-white.svg" />
+                        <SocialButton src="/assets/social/twitter-white.svg" />
+                    </Social>
+                    <Store>
+                        <StoreButton src="/assets/store/app-store.svg" />
+                        <StoreButton src="/assets/store/play-store.svg" />
+                        <StoreButton src="/assets/store/windows-store.svg" />
+                    </Store>
+                </FooterButtons>
             </FooterContent>
         </FooterWrapper>
     )
--- src/Components/Footer/Styled.js
+++ src/Components/Footer/Styled.js
@@ -15,3 +15,30 @@ display: flex;
 flex-direction: column;
 justify-content: space-between;
 `
+
+export const FooterTextContent = styled.div``
+
+export const FooterLinks = styled.div`
+display: flex;
+margin-bottom: 20px;
+`
+
+export const Copyright = styled.div`
+font-family: Raleway;
+font-size: 12px;
+font-weight: 300;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: normal;
+color: #babfc3;
+`
+
+export const FooterButtons = styled.div`
+display: flex;
+justify-content: space-between;
+`
+
+export const Social = styled.div``
+
+export const Store = styled.div``
