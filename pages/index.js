cd4270018a60eadd1f07584994fdcda0a3d0aa10
mhrrmk <mkocadere@gmail.com>
2020-02-23 01:55:37 +0300

    use components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
-import {Template, Title} from "../src/Components"
+import { Template, Title, HomeContent, HomeCard } from "../src/Components"
 
-export default function Home(){
+export default function Home() {
     return (
-        <div className="home">
-            <Template>
-                <Title>Popular Titles</Title>
-                Content
-            </Template>
-        </div>
+        <Template>
+            <Title>Popular Titles</Title>
+            <HomeContent>
+                <HomeCard title="series" href="/series" />
+                <HomeCard title="movies" href="/movies" />
+            </HomeContent>
+        </Template>
     )
 }
