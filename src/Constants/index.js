9d48f3ff8cfc184a6f7a39cdea851a47cd34d54e
mhrrmk <mkocadere@gmail.com>
2020-02-22 18:42:52 +0300

    put footer links and copyright text to constants

---
+++ src/Constants/index.js
@@ -0,0 +1,22 @@
+export const copyright = "Copyright © 2016 DEMO Streaming. All Rights Reserved."
+
+export const footerLinks = [
+    {
+        title: "Home"
+    },
+    {
+        title: "Terms and Conditions"
+    },
+    {
+        title: "Privacy Policy"
+    },
+    {
+        title: "Collection Statement"
+    },
+    {
+        title: "Help"
+    },
+    {
+        title: "Manage Account"
+    }
+]
