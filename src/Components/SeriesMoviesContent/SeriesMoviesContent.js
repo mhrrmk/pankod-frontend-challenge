9834cc1d51dd1142e7c324b809b84ba03d00b6dd
mhrrmk <mkocadere@gmail.com>
2020-02-24 01:38:17 +0300

    update SeriesMoviesContent to handle error and loading states with useEffect.

--- src/Components/SeriesMoviesContent/SeriesMoviesContent.js
+++ src/Components/SeriesMoviesContent/SeriesMoviesContent.js
@@ -1,4 +1,38 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { SeriesMoviesContentWrapper, SeriesMoviesStyledContent } from "./Styled"
+import { fetchData } from "../../Utils"
+import { DataContent } from "../"
+import { useDataState } from "../../State"
 
-export function SeriesMoviesContent(props) {
+export function SeriesMoviesContent({ type }) {
+    console.log("type:", type)
+
+    const [state, dispatch, actions, dataStates] = useDataState()
+    console.log("DataState: ", state)
+
+    useEffect(() => {
+        async function fetchMovies() {
+            try {
+                const fetchedData = await fetchData(type)
+                dispatch({ type: actions.SET_DATA, payload: fetchedData })
+            } catch (error) {
+                console.log(error)
+                dispatch({ type: actions.SET_ERROR })
+            }
+        }
+        fetchMovies()
+    }, [])
+
+    return (
+        <SeriesMoviesContentWrapper>
+            <SeriesMoviesStyledContent>
+                {state.dataState === dataStates.fetched &&
+                    <DataContent data={state.data} />}
+                {state.dataState === dataStates.loading &&
+                    <div>loading...</div>}
+                {state.dataState === dataStates.error &&
+                    <div>Oops... Something went wrong. You hit a 30% prabability. Refresh page :{")"}</div>}
+            </SeriesMoviesStyledContent>
+        </SeriesMoviesContentWrapper>
+    )
 }
--- src/Components/SeriesMoviesContent/Styled.js
+++ src/Components/SeriesMoviesContent/Styled.js
@@ -1,1 +1,15 @@
 import styled from "styled-components"
+import {Content} from "../Styled"
+
+export const SeriesMoviesContentWrapper = styled.div`
+display: flex;
+flex-grow: 1;
+`
+
+export const SeriesMoviesStyledContent = styled(Content)`
+display: flex;
+flex-direction: column;
+margin-top: 33px;
+margin-bottom: 33px;
+`
+
