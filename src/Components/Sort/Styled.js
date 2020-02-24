0bb5cf754d0087cd94291a3203093fd1ef0c65fc
mhrrmk <mkocadere@gmail.com>
2020-02-24 22:31:21 +0300

    improve css for responsiveness

--- src/Components/Sort/Styled.js
+++ src/Components/Sort/Styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
 
 export const Select = styled.select`
-width: 285px;
+flex-grow: 1;
+max-width: 285px;
 height: 37px;
 box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.18);
 border: solid 0 #b6b6b6;
@@ -21,16 +22,3 @@ option {
 }
 
 `
-// option {
-//     box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.18);
-//     border: solid 0 #b6b6b6;
-//     background-color: #ffffff;
-//     font-family: Raleway;
-//     font-size: 13px;
-//     font-weight: normal;
-//     font-stretch: normal;
-//     font-style: normal;
-//     line-height: normal;
-//     letter-spacing: normal;
-//     color: #414141;
-// }
