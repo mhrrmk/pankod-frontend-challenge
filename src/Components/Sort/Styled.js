0e850d9e6de679b684cd11efd49501fdf6d49dc5
mhrrmk <mkocadere@gmail.com>
2020-02-24 14:25:17 +0300

    remove unnecessary css

--- src/Components/Sort/Styled.js
+++ src/Components/Sort/Styled.js
@@ -14,19 +14,23 @@ font-style: normal;
 line-height: normal;
 letter-spacing: normal;
 color: #b4b4b4;
+padding-left: 16px;
 
 option {
-    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.18);
-    border: solid 0 #b6b6b6;
-    background-color: #ffffff;
-    font-family: Raleway;
-    font-size: 13px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
     color: #414141;
 }
 
 `
+// option {
+//     box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.18);
+//     border: solid 0 #b6b6b6;
+//     background-color: #ffffff;
+//     font-family: Raleway;
+//     font-size: 13px;
+//     font-weight: normal;
+//     font-stretch: normal;
+//     font-style: normal;
+//     line-height: normal;
+//     letter-spacing: normal;
+//     color: #414141;
+// }
